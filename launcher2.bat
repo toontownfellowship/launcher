d4a90d5e0bd4d7b8f2483c5925069893e52e087a
Josh Siegel <jsiegel@stormbirds.org>
2015-01-13 05:25:15 -0600

    make it less chattery

--- launcher2.bat
+++ launcher2.bat
@@ -1,10 +1,10 @@
 @cd ..\src
 @..\git\bin\git fetch
-@..\git\bin\git reset --hard
-@..\git\bin\git checkout test-v1.2.5 
+@..\git\bin\git reset --hard --quiet
+@..\git\bin\git checkout test-v1.2.5  --quiet
 @cd ..\resources
 @..\git\bin\git fetch
-@..\git\bin\git reset --hard
-@..\git\bin\git checkout test-v1.2.4
+@..\git\bin\git reset --quiet --hard --quiet
+@..\git\bin\git checkout test-v1.2.4 --quiet
 @cd ..
 @launcher\launcher3.bat
