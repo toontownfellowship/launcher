c23954bbb89f4015a3ace89366934a46e02c4a40
Josh Siegel <jsiegel@stormbirds.org>
2015-01-13 21:58:00 -0600

    fix typo

--- launcher.bat
+++ launcher.bat
@@ -1,5 +1,5 @@
 @echo looking for launcher updates..
 @cd launcher
-@..\git\bin\git pull  --quiet
-@..\git\bin\git reset --hard  --quiet
+@..\git\bin\git pull
+@..\git\bin\git reset --hard
 @launcher2.bat
