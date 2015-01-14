6efe50a251669d8718a1f90c69559bc6cc95db38
Josh Siegel <jsiegel@stormbirds.org>
2015-01-14 05:31:14 -0600

    less clutter

--- launcher3.bat
+++ launcher3.bat
@@ -22,5 +22,5 @@ SET dtStamp24=%date:~-4%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%%time:~6,2%
 
 if "%HOUR:~0,1%" == " " (SET dtStamp=%dtStamp9%) else (SET dtStamp=%dtStamp24%)
 
-..\panda3d-1.9.0\python\ppython.exe -m toontown.toonbase.ClientStart > ..\logs\ttlog.%dtStamp% 2>&1
+start /b ..\panda3d-1.9.0\python\ppython.exe -m toontown.toonbase.ClientStart > ..\logs\ttlog.%dtStamp% 2>&1
 
