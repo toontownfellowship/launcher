66ba4b49fc78d4f5d0441d05c086af71e730f65e
Josh Siegel <jsiegel@stormbirds.org>
2015-01-13 22:24:39 -0600

    try this again

--- installer.bat
+++ installer.bat
@@ -1,7 +1,7 @@
 @echo retrieving src tree.. this will take a while...
-@git\bin\git clone --branch test-v1.2.5 https://github.com/toontownfellowship/src.git
+@git\bin\git clone https://github.com/toontownfellowship/src.git
 @echo retrieving resource tree.. this will take longer..
-@git\bin\git clone --branch test-v1.2.4 https://github.com/toontownfellowship/resources.git
+@git\bin\git clone https://github.com/toontownfellowship/resources.git
 @echo retrieving launcher
 @git\bin\git clone https://github.com/toontownfellowship/launcher.git
 @copy launcher\launcher.bat .
--- launcher2.bat
+++ launcher2.bat
@@ -3,9 +3,11 @@
 @..\git\bin\git fetch
 @..\git\bin\git reset --hard --quiet
 @..\git\bin\git checkout master --quiet
+@..\git\bin\git pull --quiet
 @cd ..\resources
 @..\git\bin\git fetch
 @..\git\bin\git reset --quiet --hard --quiet
 @..\git\bin\git checkout master --quiet
+@..\git\bin\git pull --quiet
 @cd ..
 @launcher\launcher3.bat
