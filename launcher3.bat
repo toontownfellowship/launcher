b6f775c74cc73806cc269108cf09627229eb0f8f
Josh Siegel <jsiegel@stormbirds.org>
2015-01-14 05:20:38 -0600

    lets log this

--- launcher3.bat
+++ launcher3.bat
@@ -16,5 +16,11 @@ echo Username: %ttiUsername%
 echo Gameserver: %TTI_GAMESERVER%
 echo ===============================
 
-..\panda3d-1.9.0\python\ppython.exe -m toontown.toonbase.ClientStart
-pause
+SET HOUR=%time:~0,2%
+SET dtStamp9=%date:~-4%%date:~4,2%%date:~7,2%_0%time:~1,1%%time:~3,2%%time:~6,2% 
+SET dtStamp24=%date:~-4%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%%time:~6,2%
+
+if "%HOUR:~0,1%" == " " (SET dtStamp=%dtStamp9%) else (SET dtStamp=%dtStamp24%)
+
+..\panda3d-1.9.0\python\ppython.exe -m toontown.toonbase.ClientStart > ..\logs\ttlog.%dtStamp% 2>&1
+
