86173ba024a37b2d982451d2acd00334d6034335
enigmeow <jsiegel@stormbirds.org>
2015-01-18 08:28:57 -0600

    new stuff

--- launcher2.bat
+++ launcher2.bat
@@ -6,8 +6,9 @@
 @..\git\bin\git pull --quiet
 @cd ..\resources
 @..\git\bin\git fetch
-@..\git\bin\git reset --quiet --hard --quiet
+@..\git\bin\git reset --quiet --hard
 @..\git\bin\git checkout master --quiet
 @..\git\bin\git pull --quiet
 @cd ..
-@launcher\launcher3.bat
+rem @launcher\launcher3.bat
+@launcher\newlaunch.hta
---
+++ lstub.bat
@@ -0,0 +1,11 @@
+@echo off
+cd src
+
+SET HOUR=%time:~0,2%
+SET dtStamp9=%date:~-4%%date:~4,2%%date:~7,2%_0%time:~1,1%%time:~3,2%%time:~6,2% 
+SET dtStamp24=%date:~-4%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%%time:~6,2%
+
+if "%HOUR:~0,1%" == " " (SET dtStamp=%dtStamp9%) else (SET dtStamp=%dtStamp24%)
+
+start /b ..\panda3d-1.9.0\python\ppython.exe -m toontown.toonbase.ClientStart > ..\logs\ttlog.%dtStamp% 2>&1
+
