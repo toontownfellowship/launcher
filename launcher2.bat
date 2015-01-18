c5ab5465cb30bea8b679e9f851681c98ba19cef1
enigmeow <jsiegel@stormbirds.org>
2015-01-18 08:34:00 -0600

    improve reset

--- launcher2.bat
+++ launcher2.bat
@@ -1,12 +1,12 @@
 @echo looking for game updates
 @cd ..\src
 @..\git\bin\git fetch
-@..\git\bin\git reset --hard --quiet
+@..\git\bin\git reset --hard origin/master --quiet
 @..\git\bin\git checkout master --quiet
 @..\git\bin\git pull --quiet
 @cd ..\resources
 @..\git\bin\git fetch
-@..\git\bin\git reset --quiet --hard
+@..\git\bin\git reset --hard  origin/master --quiet
 @..\git\bin\git checkout master --quiet
 @..\git\bin\git pull --quiet
 @cd ..
